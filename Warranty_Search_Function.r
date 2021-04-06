aac7fa15fc840242f9bfaf3a48e806e4c3058d72
Waylan49 <39176203+Waylan49@users.noreply.github.com>
2021-04-06 16:45:01 -0500

    Add if-else loop
    Adding if-else loop to classify the runway product and 3 part product separately.

--- Warranty_Search_Function.r
+++ Warranty_Search_Function.r
@@ -11,6 +11,7 @@ iReturn_Data<-do.call(rbind, lapply(filename, read.csv, header=TRUE))
 myda<-iReturn_Data[, c("DISP.LOCATION", "DATE.SENT", "PART.SERNO")]
 myda$DATE.SENT<-as.POSIXct(strptime(myda$DATE.SENT, "%m/%d/%Y %H:%M"))
 myda<-myda %>% arrange(desc(DATE.SENT))
+myda$PART.SERNO<-substr(myda$PART.SERNO, 6, 14)
 myda<-myda %>% distinct(PART.SERNO, .keep_all = TRUE)
 mydaF<-data.frame(myda$PART.SERNO, myda$DISP.LOCATION)
 mydaF$myda.PART.SERNO<-as.character(mydaF$myda.PART.SERNO)
@@ -28,17 +29,30 @@ wcheck<-function(SN){
         SN1<-substr(SN, 6, 14)
         loc<-grep(SN1, mydaF$myda.PART.SERNO)
         warranty<-mydaF$myda.DISP.LOCATION[loc]
-        ifelse(warranty=="RT23", paste(SN, " is ", "In Warranty"), paste(SN, " is ", "Out of Warranty"))
+        final_w<-ifelse(warranty=="RT23", "IW", "OOW")
+        if(length(final_w)<1){
+                data.frame(Serial_Number=SN, Node_SN="NA", Warranty="No Record Found")
+        }
+        else{ 
+                data.frame(Serial_Number=SN, Node_SN="NA", Warranty=final_w)
+        }
+  
         }
         else{
              new_SN<-Assy$SPS.[loc<-which(Assy$Assy.==SN)]
              loc<-which(mydaF$myda.PART.SERNO==new_SN)
              warranty<-mydaF$myda.DISP.LOCATION[loc]
-             ifelse(warranty=="RT23", paste(SN, " is 3PAR: ", new_SN , " which is In Warranty"), 
-                    paste(SN, " is 3PAR: ", new_SN , " which is Out of Warranty"))
+             final_w<-ifelse(warranty=="RT23", "IW", "OOW")
+        
+        if(length(new_SN)<1){
+                data.frame(Serial_Number=SN, Node_SN="No Record Found", Warranty="No Record Found")
+        }else if(length(final_w)<1){
+                data.frame(Serial_Number=SN, Node_SN=new_SN, Warranty="No Record Found")
+        }else{
+                data.frame(Serial_Number=SN, Node_SN=new_SN, Warranty=final_w)
+        }
         }
 }
-
 
 
 
